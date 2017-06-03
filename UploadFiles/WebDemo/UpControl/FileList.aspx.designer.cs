aa2b0ffacd9e3cbc3f3ecdd8828e5202daece7f5
SeaLee <654029739@qq.com>
2017-06-03 16:49:37 +0800

    文件上传功能

---
+++ UploadFiles/WebDemo/UpControl/FileList.aspx.designer.cs
@@ -0,0 +1,51 @@
+﻿//------------------------------------------------------------------------------
+// <自动生成>
+//     此代码由工具生成。
+//
+//     对此文件的更改可能导致不正确的行为，如果
+//     重新生成代码，则所做更改将丢失。
+// </自动生成>
+//------------------------------------------------------------------------------
+
+namespace WebDemo.UpControl {
+    
+    
+    public partial class FileList {
+        
+        /// <summary>
+        /// form1 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        
+        /// <summary>
+        /// lb_DownLoad 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.LinkButton lb_DownLoad;
+        
+        /// <summary>
+        /// lb_YS 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.LinkButton lb_YS;
+        
+        /// <summary>
+        /// DemoRepeater 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Repeater DemoRepeater;
+    }
+}
---
+++ UploadFiles/WebDemo/UpControl/UploadDemo.aspx.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+
+namespace WebDemo.UpControl
+{
+    public partial class UploadDemo : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                //string aa = "{\"status\": 0, \"msg\": \"不允许上传类型的文件！\"}";
+                //Page.ClientScript.RegisterStartupScript(Page.ClientScript.GetType(), "msg", "<script>alert("+aa+");</script>");
+                //Response.Write();
+            }
+         
+        }
+
+        protected void lbSave_Click(object sender, EventArgs e)
+        {
+            FunctionDemo.BLL.Files filesBLL = new FunctionDemo.BLL.Files();
+            FunctionDemo.Model.Files filesModel = new FunctionDemo.Model.Files();
+            filesModel.FileName = txtTitle.Value.Trim();
+            filesModel.FilePath = txtFileUrl.Text.Trim();
+            filesModel.FileSize = (int.Parse(txtFileSize.Value.Trim()) / 1024) + "K";
+            filesModel.UpdateTime = DateTime.Now;
+            int i = txtFileUrl.Text.Trim().LastIndexOf(".") + 1;
+            string Name = txtFileUrl.Text.Trim().Substring(i);
+            tool.Upload up = new tool.Upload();
+            if (up.IsImage(Name.ToLower()))
+            {
+                filesModel.FileType = 0;
+            }
+            else
+            {
+                filesModel.FileType = 1;
+            }
+            if (filesBLL.Add(filesModel)>0)
+            {
+                Page.ClientScript.RegisterStartupScript(Page.ClientScript.GetType(), "myscript", "<script> alert('上传成功');</script>");
+                txtTitle.Value = "";
+                txtFileUrl.Text = "";
+                txtFileSize.Value = "";
+                return;
+            }
+            else
+            {
+                Page.ClientScript.RegisterStartupScript(Page.ClientScript.GetType(), "myscript", "<script> alert('上传失败');</script>");
+            }
+        }
+    }
+}
