366d57d1f2fa7e509750d539b77435416063173a
SeaLee <654029739@qq.com>
2017-06-08 16:41:51 +0800

    文件上传第二种方法JS

---
+++ UploadFiles/WebDemo/JS/UpControl2/jquery.leadinupload.js
@@ -0,0 +1,59 @@
+﻿/// <reference path="jquery-1.11.1.min.js" />
+
+//当我点击上传的时候，这个事件触发，然后主动让下面的事件发生
+var btnfileaclick = function ($this) {
+
+    $this.parent().siblings(".fileinput").find("input[type=file]").click();
+
+}
+
+
+var fileonchange = function ($this,formId) {
+
+    var fileid = $this.attr("id"); //文件上传ID
+    var txtid = $this.parent().siblings(".txtfilename").find("input[type=text]").attr("id"); //路径ID
+    //formId是form的Id   ajaxSubmit的前提需要引用jquery.form.min.js
+    $("#" + formId).ajaxSubmit({
+
+        beforeSubmit: function (formData, jqForm, options) {
+        },
+        success: function (data) {
+            $("#" + data.txtName).val(data.filePath);   //文本框赋值   data有id和value
+        },
+        error: function (data, status, e) {
+            alert("上传失败");
+        },
+        url: "/Tools/Update.ashx",
+        type: "post", //post提交
+        data: { fileid: fileid, txtid: txtid },
+        dataType: "json",
+        timeout: 60000
+    });
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
