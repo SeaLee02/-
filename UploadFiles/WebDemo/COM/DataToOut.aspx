26261cb7406cd9fd71d24c2749ce3d38bd0da347
SeaLee <654029739@qq.com>
2017-06-28 14:03:29 +0800

    数据导出

---
+++ UploadFiles/WebDemo/COM/DataToOut.aspx
@@ -0,0 +1,110 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="DataToOut.aspx.cs" Inherits="WebDemo.COM.DataToOut" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+      <link href="../CSS/TableCSS.css" rel="stylesheet" />
+    <link href="../CSS/dataTolead.css" rel="stylesheet" />
+</head>
+<body>
+    <form id="form1" runat="server">
+       <div style="margin-left: 100px;">
+           <asp:LinkButton CssClass="add" ID="lnkData" runat="server" OnClientClick="return Test()" OnClick="lnkData_Click">导出<br />
+                数据</asp:LinkButton>
+           <asp:HiddenField ID="hfID" runat="server" />
+        </div>
+        <br />
+        <br />
+        <div class="tableDiv">
+            <asp:Repeater ID="DemoRepeater" runat="server">
+                <HeaderTemplate>                  
+                    <table>
+                        <tbody>
+                            <tr>
+                                 <th width="40">
+                                    <input type="checkbox" id="checkAll" name="checkAll" />
+                                </th>
+                                <th>序号
+                                </th>
+                                <th>姓名
+                                </th>
+                                <th>性别
+                                </th>
+                                <th>年龄
+                                </th>
+                                <th>电话号码</th>
+                            </tr>                     
+                  
+                </HeaderTemplate>
+                <ItemTemplate>
+                  <tr>
+                       <td align="center">
+                                <input type="checkbox" name="chk" value="<%#Eval("ID") %>" onclick="ck();" />
+                            </td>
+                      <td><%#RowIndex++ %></td>
+                      <td><%#Eval("Name") %></td>
+                      <td><%#Eval("Sex") %></td>
+                      <td><%#Eval("Age") %></td>
+                      <td><%#Eval("Tel") %></td>
+                   </tr>  
+                </ItemTemplate>
+                <FooterTemplate>
+                       </tbody></table>
+                </FooterTemplate>
+            </asp:Repeater>
+        </div>
+        <script src="../JS/jquery-1.11.0.min.js"></script>
+        <script src="../JS/jquery.tableui.js"></script>
+        <script>
+        
+            $(function () {
+                $("table").tableUI();             
+            });
+
+            //全选/不全选
+            $("#checkAll").click(function () {
+                var f = $("#checkAll").is(":checked")
+                if (f) {
+                    $("input[name=chk]:checkbox").prop("checked", true);
+                } else {
+                    $("input[name=chk]:checkbox").prop("checked", false);
+                }
+            });
+
+            //单选
+            var d = $("input[name=chk]:checkbox").length;
+            $("input[name=chk]:checkbox").click(function () {
+                var dd = $("input[name=chk]:checked").length;
+                if (parseInt(d) === parseInt(dd)) {
+                    $("#checkAll").prop("checked", true);
+                } else {
+                    $("#checkAll").prop("checked", false);
+                }
+            });
+
+            //取值
+            function Test() {             
+                var ids = "";            
+                var dd = $("input[name=chk]:checked");               
+                dd.each(function () {
+                        ids += $(this).val() + ",";                       
+                });
+                if (ids=="") {
+                    alert("请选择导入的数据");
+                    return false;
+                }
+                ids = ids.substring(0, ids.length - 1);
+                $("#hfID").val(ids);
+                return true;
+            };
+       
+
+           
+
+        </script>
+    </form>
+</body>
+</html>
---
+++ UploadFiles/WebDemo/COM/DataToOut.aspx.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Aspose.Cells;
+using System.Data;
+
+namespace WebDemo.COM
+{
+    public partial class DataToOut : System.Web.UI.Page
+    {
+        public int RowIndex
+        {
+            get
+            {
+                object o = ViewState["RowIndex"];
+                return o == null ? 1 : Convert.ToInt32(o);
+            }
+            set { ViewState["RowIndex"] = value; }
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                Bings();
+            }
+        }
+
+        private void Bings()
+        {
+            DemoRepeater.DataSource = new FunctionDemo.BLL.DataToLead().GetList("").Tables[0];
+            DemoRepeater.DataBind();
+        }
+
+        protected void lnkData_Click(object sender, EventArgs e)
+        {
+            DataTable ds = new FunctionDemo.BLL.DataToLead().GetList(" ID in (" + hfID.Value + ")").Tables[0];
+            if (ds.Rows.Count > 0)
+            {
+                //创建一个excel表格
+                Workbook workbook = new Workbook();
+                Worksheet sheet = workbook.Worksheets[0] as Worksheet; //工作薄
+                Cells cells = sheet.Cells;//取到所以的列
+                //然后往cell里面插入数据  
+                sheet.FreezePanes(1, 1, 1, 0);//冻结第一行
+                cells[0, 0].PutValue("姓名");
+                cells[0, 1].PutValue("性别");
+                cells[0, 2].PutValue("年龄");
+                cells[0, 3].PutValue("电话号码");
+                cells[0, 4].PutValue("标记");
+
+                Aspose.Cells.Style style1 = workbook.Styles[workbook.Styles.Add()]; //设置样式  然后进行设置
+                style1.HorizontalAlignment = Aspose.Cells.TextAlignmentType.Center;//居中
+                //列宽
+                cells.SetColumnWidth(0, 20);
+                cells.SetColumnWidth(1, 20);
+                cells.SetColumnWidth(2, 20);
+                cells.SetColumnWidth(3, 50);
+                cells.SetColumnWidth(4, 20);
+                for (int i = 0; i < ds.Rows.Count; i++)
+                {
+                    cells.SetRowHeight(i, 20); //行高
+                    //赋值和样式
+                    cells[1 + i,0].PutValue(ds.Rows[i]["Name"].ToString());
+                    cells[1 + i,0].SetStyle(style1);
+                    cells[1 + i,1].PutValue(ds.Rows[i]["Sex"].ToString());
+                    cells[1 + i,1].SetStyle(style1);
+                    cells[1 + i,2].PutValue(ds.Rows[i]["Age"].ToString());
+                    cells[1 + i,2].SetStyle(style1);
+                    cells[1 + i,3].PutValue(ds.Rows[i]["Tel"].ToString());
+                    cells[1 + i,3].SetStyle(style1);
+                    cells[1 + i,4].PutValue(ds.Rows[i]["Remark"].ToString());
+                    cells[1 + i,4].SetStyle(style1);
+                }
+                Aspose.Cells.Style style = workbook.Styles[workbook.Styles.Add()]; //设置样式
+                style.HorizontalAlignment = TextAlignmentType.Center;//居中
+                style.ForegroundColor = System.Drawing.Color.Khaki;//背景样式
+                style.Pattern = BackgroundType.Solid; //枚举类型  模式 实线
+                cells[0,0].SetStyle(style); //第一行设计样式  头
+                cells[0, 1].SetStyle(style); 
+                cells[0, 2].SetStyle(style); 
+                cells[0, 3].SetStyle(style); 
+                cells[0, 4].SetStyle(style); 
+                System.IO.MemoryStream ms = workbook.SaveToStream();//生成流
+                byte[] by = ms.ToArray();//生成字节好下载
+
+                string fileName = "导出数据" + DateTime.Now.ToString("yyyyMMddHHmmss")+".xls";//文件名
+                Response.ContentType = "application/octet-stream";//文件下载
+                //以字节流形式下载excel   上面下面两种类型都可以
+                //Response.ContentType = "application/vnd.ms-excel";
+                //编码 
+                Response.AppendHeader("Content-Disposition", "attachment;filename=" + HttpUtility.UrlEncode(fileName,System.Text.Encoding.UTF8));
+                Response.BinaryWrite(by);
+                Response.Flush();
+                Response.End();
+            }
+        }
+    }
+}
